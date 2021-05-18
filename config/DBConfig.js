b0601ed4a66ea1c4dfd4842e428ef37b691483c8
whai hoe <kwokwhaihoe@gmail.com>
2021-05-18 17:38:00 +0800

    sidebar and navbar in partials and fixed css for login

--- config/DBConfig.js
+++ config/DBConfig.js
@@ -1,13 +1,12 @@
 // Bring in Sequelize
 const Sequelize = require('sequelize');
-    // Bring in db.js which contains database name, username and password
-    const db = require('./db');
-    // Instantiates Sequelize with database parameters
-    const sequelize = new Sequelize(db.database, db.username, db.password, {
+// Bring in db.js which contains database name, username and password
+const db = require('./db');
+// Instantiates Sequelize with database parameters
+const sequelize = new Sequelize(db.database, db.username, db.password, {
     host: db.host, // Name or IP address of MySQL server
     dialect: 'mysql', // Tells squelize that MySQL is used
     operatorsAliases: false,
-    
     define: {
         timestamps: false // Don't create timestamp fields in database
     },
@@ -16,6 +15,6 @@ const Sequelize = require('sequelize');
         min: 0,
         acquire: 30000,
         idle: 10000
-},
+    },
 });
 module.exports = sequelize;
