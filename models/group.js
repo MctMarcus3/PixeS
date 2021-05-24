92e79fd01ec0fc7453cf75d6f99fab8b0918b27d
qikuamai <77825518+qikuamai@users.noreply.github.com>
2021-05-24 15:12:06 +0800

    shaun

--- models/group.js
+++ models/group.js
@@ -12,18 +12,4 @@ const Group = db.define('group', {
     }
 });
 
-module.exports = Group;const Sequelize = require("sequelize");
-const db = require("../config/DBConfig");
-/* Creates a user(s) table in MySQL Database.
-Note that Sequelize automatically pleuralizes the entity name as the table name
-*/
-const Group = db.define("group", {
-  name: {
-    type: Sequelize.STRING,
-  },
-  grp_id: {
-    type: Sequelize.STRING,
-  },
-});
-
 module.exports = Group;
