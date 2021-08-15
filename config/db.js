01a0ff7308ccfb0528718e336e4735f29e94502b
qikuamai <77825518+qikuamai@users.noreply.github.com>
2021-08-15 15:13:31 +0800

    grp

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 module.exports = {
     host: 'localhost',
-    database: 'vidjot',
-    username: 'itp211',
-    password: 'itp211'
+    database: 'pixes',
+    username: 'pixesroot',
+    password: 'pixesroot'
 }
